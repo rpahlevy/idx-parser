f4964ff5f0fb809040394aad87b71158ad844b10
Rio Pahlevy <43176811+rpahlevy@users.noreply.github.com>
2024-07-26 20:33:20 +0700

    link indices to company

--- main.py
+++ main.py
@@ -16,28 +16,26 @@ _indices = [
   'JII70',
   'LQ45',
 ]
-
-# Function to dynamically detect and extract table data
-def extract_table_data(sheet):
-  table_data = []
-  in_table = False
-  
-  for row in sheet.iter_rows(values_only=True):
-    # Check if the second cell in the row is a number or the row contains valid data
-    if isinstance(row[1], (int, float)) and (row[1] is not None and row[1] != 'No.'):
-      in_table = True
-      table_data.append(row)
-    elif in_table:
-      break
-          
-  return table_data
+_doc_indices = 'indices'
+_doc_companies = 'companies'
+_field_indices = 'indices'
+_field_companies = 'companies'
 
 @click.command()
 def indices():
-  doc_ref = db.collection('indices')
-  docs = doc_ref.stream()
+  docs = get_indices()  
   for doc in docs:
     print(f'{doc.id}')
+
+def get_indices():
+  doc_ref = db.collection(_doc_indices)
+  docs = doc_ref.stream()
+  return docs
+
+def get_companies():
+  doc_ref = db.collection(_doc_companies)
+  docs = doc_ref.stream()
+  return docs
 
 # Command to extract data from Excel file
 @click.command()
@@ -60,18 +58,84 @@ def extract(index, file, sync):
     # Upload data to Firestore
     if sync:
       print('Sync to Firestore...')
-      store_data_in_firestore(companies, index)
+      store_company_in_index(companies, index)
   except Exception as e:
     print(f"Error: {e}")
     sys.exit(1)
 
-def store_data_in_firestore(data, index):
+# Function to dynamically detect and extract table data
+def extract_table_data(sheet):
+  table_data = []
+  in_table = False
+  
+  for row in sheet.iter_rows(values_only=True):
+    # Check if the second cell in the row is a number or the row contains valid data
+    if isinstance(row[1], (int, float)) and (row[1] is not None and row[1] != 'No.'):
+      in_table = True
+      table_data.append(row)
+    elif in_table:
+      break
+          
+  return table_data
+
+def store_company_in_index(data, index):
   """Stores the extracted data in a Firestore collection."""
-  collection_name = 'indices'
-  doc_ref = db.collection(collection_name).document(f'{index}')  # Generate document ID with index
+  doc_ref = db.collection(_doc_indices).document(f'{index}')  # Generate document ID with index
   doc_ref.update({
-    'companies': data
+    _field_companies: data
   })
+
+# Command to link indices to companies
+@click.command()
+@click.option('--clear', is_flag=True, prompt='Clear previous indices list in each company?')
+def link_indices(clear):
+  if (clear):
+    clear_company_indices()
+
+  # get indices collection
+  indices = get_indices()
+  companies = {}
+  for i in indices:
+    id = i.id
+    index = i.to_dict()
+
+    # filter out indices without companies
+    if _field_companies not in index:
+      continue
+
+    # foreach company in the index,
+    # add it to companies dict and append the index to it
+    for company in index[_field_companies]:
+      if company not in companies:
+        companies[company] = {
+          _field_indices: []
+        }
+      companies[company][_field_indices].append(id)
+
+  docs = db.collection(_doc_companies).stream()
+  batch = db.batch()
+
+  for doc in docs:
+    if doc.id not in companies:
+      continue
+
+    doc_ref = doc.reference
+    batch.update(doc_ref, companies[doc.id])
+
+  batch.commit()
+
+def clear_company_indices():
+  """Clears the specified field for all documents in a collection using batch writes."""
+
+  docs = db.collection(_doc_companies).stream()
+  batch = db.batch()
+
+  for doc in docs:
+    doc_ref = doc.reference
+    batch.update(doc_ref, {_field_indices: []})
+
+  batch.commit()
+
 
 @click.group()
 def cli():
@@ -79,6 +143,7 @@ def cli():
 
 cli.add_command(extract)
 cli.add_command(indices)
+cli.add_command(link_indices)
 # cli.add_command(clear_db)
 # cli.add_command(parse_sync_db)
 
